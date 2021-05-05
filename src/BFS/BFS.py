2bcdb92c04991d76ab842adf3bcc4e6c7c916159
Stanciu Vlad <vladcnitv@yahoo.com>
2021-05-05 19:07:15 +0300

    BFS e aproape gata mai are nevoie de o legenda.

--- src/BFS/BFS.py
+++ src/BFS/BFS.py
@@ -12,7 +12,7 @@ def ReadGraph():
                 G.add_edge(i, j, length = line[j])
 
     for i in range(nodes):
-        G.nodes[i]['degree'] = 0
+        G.nodes[i]['degree'] = -1
     
     start = int(f.readline())
 
@@ -20,13 +20,15 @@ def ReadGraph():
 
 def DrawGraph(G, color_map):
     node_labels = nx.get_node_attributes(G, 'degree')
-    plt.subplots(121, figsize=(5, 8))
+    plt.figure(num="BFS", figsize=(11, 7))
+    plt.subplot(121)
+    plt.title("Initial graph")
     pos = nx.spring_layout(G)
-    #nx.draw_networkx(G, pos=pos, node_color=color_map, with_labels=True,label=node_labels)
     nx.draw(G, pos, node_color=color_map, with_labels=True)
     edge_labels = dict([((u,v,), d['length']) for u, v, d in G.edges(data = True)])
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3, font_size=11)
-    plt.subplots(122, figsize=(5, 8))
+    plt.subplot(122)
+    plt.title("Degree graph")
     nx.draw(G, pos, node_color=color_map, with_labels=True, labels=node_labels)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3, font_size=11)
     plt.show()
@@ -34,15 +36,15 @@ def DrawGraph(G, color_map):
 def BFS(G, start):
     queue = []
     queue.append(start)
-    G.nodes[start]['degree'] = 1
+    G.nodes[start]['degree'] = 0
     color_map = []
-    color_map.append('blue')
+    color_map.append('green')
     while len(queue) > 0:
         front = queue.pop(0)
         print(front)
         ng = G.adj[front]
         for i in ng:
-            if G.nodes[i]['degree'] == 0:
+            if G.nodes[i]['degree'] == -1:
                 queue.append(i)
                 G.nodes[i]['degree'] = G.nodes[front]['degree'] + 1
                 if G.nodes[i]['degree'] % 2 == 0:
