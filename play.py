27f78db44d8095d80f1aeab6c988ff73adfe4600
bogdan <bogdan.butnariu@stud.acs.upb.ro>
2021-05-06 19:23:31 +0300

    play.py is done

--- play.py
+++ play.py
@@ -4,6 +4,17 @@
 import os
 import time
 
+def load_message(mes, n):
+    dots = "."
+    for i in range(n):
+        print(mes + dots)
+        time.sleep(0.5)
+        dots += '.'
+        if i < n-1:
+            os.system(clear)
+        else:
+            continue
+
 sleep = "sleep 10"
 clear = "clear"
 
@@ -18,7 +29,9 @@ flo = "./src/Floyd_Warshall/Floyd_Warshall.py"
 intro = "Welcome to Graph Visualization Tool! Press 'y' to continue or 'q' to exit"
 ques1 = "Do you have installed all the packages needed? [y/n]: "
 ques2 = "Select an algorithm for the graph:\n\t1: BFS\n\t2: DFS\n\t3: Dijkstra\n\t4: Floyd Warsahll\nChoice: "
-dots = "."
+
+root = int()
+n = int()
 
 algo = {
     '1' : 'BFS',
@@ -29,19 +42,13 @@ algo = {
 
 os.system(clear)
 
-for i in range(5):
-    print(intro + dots)
-    time.sleep(0.5)
-    dots += '.'
-    if i < 4:
-        os.system(clear)
-    else:
-        continue
+load_message(intro, 5)
 
 response = input()
 while True:
     if response == 'q':
         print("Bye!")
+        exit()
         break
     elif response == 'y':
         break
@@ -67,25 +74,63 @@ while True:
     if choice.isnumeric():
         
         if int(choice) > 0 and int(choice) < 5:
-            print(f"Your choice is {algo[choice]} algorithm. Please select to entry input or show example. [i/e]: ")
-            break
+            print(f"Your choice is {algo[choice]} algorithm. Please select to entry input, show example or retry. [i/e/r]: ", end="")
+            response = input()            
+            
+            if response == 'r':
+                print(ques2)
+                choice = input(ques2)
+                continue
+            else:
+                break
         else:
             choice = input("Not a valid choice! Retry: ")        
     else:
         choice = input("Not a number! Retry: ")
 
-response = input()
 while True:
     if response == 'i':
-        pass
+       
+        n = int(input("Enter the number 'n' for n*n matrix: "))
+        matrix = []
+        for i in range(n):
+            print(f"Enter the {i+1} row, with space between numbers:")
+            matrix.append(input())
+        
+        root = int(input("Enter the starting root: "))
+        
+        out = open("input.txt", "w")
+        out.write(f"{n}\n")
+        
+        for i in range(n):
+            out.write(f"{matrix[i]}\n")
+        
+        out.write(f"{root}")
+        out.close()
         break
+    
     elif response == 'e':
-        os.system(f"./src/{algo[choice]}/{algo[choice]}.py")
         break
+    
     else:
         response = input("Not a valid choice! Retry or exit [q]: ")
         if response == 'q':
             print("Bye!")
             exit()
-print("Done!")
+
+load_message("Loading", 6)
+
+print("Done! Now you have a graph!")
+
+os.system(f"./src/{algo[choice]}/{algo[choice]}.py")
+
+if response == 'i':
+    print("\nYour input was:")
+    print(f"n: {n}")
+    print('matrix:')
+    for i in range(n):
+        for j in range(n):
+            print(matrix[i][j], end = " ")
+        print()
+    print(f"Starting root: {root}")
     
