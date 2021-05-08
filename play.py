116225b7ba07cbe018405d0d1f0b0ca076ace2c3
bogdan <bogdan.butnariu@stud.acs.upb.ro>
2021-05-08 03:37:36 +0300

    comments + code review

--- play.py
+++ play.py
@@ -3,65 +3,6 @@
 
 # libraries
 import os
-import time
-
-def load_message(mes, n): # function that add n '.' after mes, used for loading screens
-    dots = "."
-    for i in range(n):
-        print(mes + dots)
-        time.sleep(0.5)
-        dots += '.'
-        if i < n-1:
-            os.system(clear)
-        else:
-            continue
-
-def entry():
-    n = int(input("Enter the number 'n' for n*n matrix: "))
-        
-    matrix = []
-    for i in range(n):
-        print(f"Enter the {i+1} row, {n} numbers are expected:")
-            
-        string = input()
-        elements = list(string.split(" "))
-        elements = [i for i in elements if i.isnumeric()] 
-            
-        while len(elements) < n or len(elements) > n:
-            print("Gresit.")
-            string = input()
-            elements = list(string.split(" "))
-            elements = [i for i in elements if i.isnumeric()]  
-        matrix.append(elements)
-        
-    root = int(input("Enter the starting root: "))
-    return matrix, n, root
-
-def input_file(matrix, n, root):
-    out = open("input.txt", "w")
-    out.write(f"{n}\n")
-    
-    for i in range(n):
-        for j in range(n):
-            if j == n - 1:
-                out.write(f"{matrix[i][j]}\n")
-            else:
-                 out.write(f"{matrix[i][j]} ")
-
-    out.write(f"{root}")
-    out.close()
-
-def input_print(matrix, n, root):
-    print("\nYour input was:")
-    print('Matrix:')    
-    for i in range(n):
-        print("   ", end='')
-        for j in range(n):
-            if j == n - 1:
-                print(f"{matrix[i][j]}")
-            else:
-                print(f"{matrix[i][j]}", end=" ")
-    print(f"Starting root: {root}")
 
 # variables
    # variables for bash commands
@@ -76,11 +17,71 @@ algo = {
     '4' : 'Floyd_Warshall'
 }
 
-# loading screen
-os.system(clear)
-load_message("Welcome to Graph Visualization Tool! Press 'y' to continue or 'q' to exit", 5)
+def load_message(string, n): # function that adds n '.' after mes, used for loading screens
+    dots = "."
+    for i in range(n):
+        print(string + dots) # first step, the message and one dot
+        os.system("sleep 0.5")
+        
+        dots += '.' # growth of dots
+        
+        if i == n-1:
+            continue
+        
+        os.system(clear)
 
-# 1st choice screen
+def entry(): # function that reads the input for matrix
+    n = int(input("Enter the number 'n' for n*n matrix: "))
+        
+    matrix = []
+    for i in range(n):
+        print(f"Enter the {i+1} row, {n} numbers are expected:")
+            
+        string = input()
+        elements = list(string.split(" "))
+        elements = [i for i in elements if i.isnumeric()] # this makes a list only with numbers
+            
+        while len(elements) < n or len(elements) > n: # condition for input, a row,  to contain 'n' numbers
+            print("Wrong input. Try again: ")
+            string = input()
+            elements = list(string.split(" "))
+            elements = [i for i in elements if i.isnumeric()] 
+        matrix.append(elements)
+        
+    root = int(input("Enter the starting root: "))
+    return matrix, n, root
+
+def input_file(matrix, n, root): # function that write the input on file
+    out = open("input.txt", "w")
+    out.write(f"{n}\n")
+    
+    for i in range(n):
+        for j in range(n):
+            if j == n - 1:
+                out.write(f"{matrix[i][j]}\n")
+            else:
+                 out.write(f"{matrix[i][j]} ")
+
+    out.write(f"{root}")
+    out.close()
+
+def input_print(matrix, n, root): # function that write the input on screen
+    print("\nYour input was:")
+    print('Matrix:')    
+    for i in range(n):
+        print("   ", end='')
+        for j in range(n):
+            if j == n - 1:
+                print(f"{matrix[i][j]}")
+            else:
+                print(f"{matrix[i][j]}", end=" ")
+    print(f"Starting root: {root}")
+
+# clearing the terminal
+os.system(clear)
+
+# 1st choice screen, yes or quit
+load_message("Welcome to Graph Visualization Tool! Press 'y' to continue or 'q' to exit", 5)
 response = input()
 while True: 
     if response == 'y':
@@ -90,10 +91,9 @@ while True:
         exit()
         break
     else:
-        print("Wrong button!")
-        response = input()
+        response = input("Wrong button!\n")
 
-
+# 2nd choice screen, yes or no
 response = input("Do you have installed all the packages needed? [y/n]: ")
 while True:
     if response == 'y':
@@ -103,30 +103,32 @@ while True:
         os.system(matplotlib)
         break    
     else:
-        print("Wrong button!")
-        response = input()
+        response = input("Wrong button!\n")
 
+# 3th choice screen, algorithm choice
 choice = input("Select an algorithm for the graph:\n\t1: BFS\n\t2: DFS\n\t3: Dijkstra\n\t4: Floyd Warsahll\nChoice: ")
 while True:
     if choice.isnumeric():
         
         if int(choice) > 0 and int(choice) < 5:
-            print(f"Your choice is {algo[choice]} algorithm. Please select to entry input, show example or retry. [i/e/r]: ", end="")
+            print(f"Your choice is {algo[choice]} algorithm. Please select to entry input, show example or retry. [i/e/r]: ", end="") # 4th choice
             response = input()            
             
             if response == 'r':
-                choice = input("Choice: ")
+                choice = input("Choice: ") # retry 3th choice
                 continue
             else:
                 break
 
         else:
-            choice = input("Not a valid choice! Retry: ")        
+            choice = input("Not a valid choice! Retry: ")
+
     else:
         choice = input("Not a number! Retry: ")
 
+# construction of input.txt, based on choices
 while True:
-    if response == 'e': 
+    if response == 'e': # example choice
         if choice == '1':
             n = 5
             matrix = [['0', '5', '10', '5', '0'],
@@ -156,7 +158,7 @@ while True:
             input_print(matrix, n, root)
         break
 
-    elif response == 'i':
+    elif response == 'i': # input choice
         matrix, n, root = entry()
         load_message("Loading", 6)
         print("Done! Now you have a graph!")
@@ -164,10 +166,11 @@ while True:
         input_print(matrix, n, root)
         break
 
-    else:
+    else: # exit choice
         response = input("Not a valid choice! Retry or exit [q]: ")
         if response == 'q':
             print("Bye!")
             exit()
 
+# command that execute the chosen algorithm
 os.system(f"./src/{algo[choice]}/{algo[choice]}.py")
